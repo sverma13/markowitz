f51dfe5c8426ad180d93da6e9ae6fb3b0bf194c5
sverma13 <57202681+sverma13@users.noreply.github.com>
2020-02-05 22:40:04 -0600

    Update markowitz.py

--- markowitz.py
+++ markowitz.py
@@ -6,6 +6,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+# Function: create a column of a stock's daily returns
+def stockReturnCol(ticker):
+    ticker = ticker + '.csv'
+    data = pd.read_csv(ticker, parse_dates=['Date'])  # import historical data in pandas dataframe
+    data = data.sort_values(by='Date')
+    data.set_index('Date', inplace=True)
+
+    data['Returns'] = data['Adj Close'].pct_change()  # create a column of daily returns
+    stock_returns = data['Returns'].dropna()  # remove entries with N/A dat
+    return stock_returns
+
+
+def randomPortfolios(num_portfolios, mean_returns, cov_matrix, risk_free_rate):
+    results = np.zeros((3, num_portfolios))
+    weights_record = []
+    for i in range(num_portfolios):
+        weights = np.random.random_sample(len(stock_port))
+        weights /= np.sum(weights)
+        weights_record.append(weights)
+        portfolio_return = np.sum(mean_returns * weights) * 252
+        portfolio_std_dev = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights))) * np.sqrt(252)
+        results[0, i] = portfolio_std_dev
+        results[1, i] = portfolio_return
+        results[2, i] = (portfolio_return - risk_free_rate) / portfolio_std_dev
+    return results, weights_record
+
+
 # List stock tickers in portfolio
 stock_port = ['AAPL', 'AMZN',
               'BA',
@@ -24,41 +52,12 @@ stock_port = ['AAPL', 'AMZN',
 # Create empty dataframe of daily stock returns matrix
 stock_ret_mat = pd.DataFrame(columns=stock_port)
 
-
-# Function: create a column of a stock's daily returns
-def stockReturnCol(ticker):
-    ticker = ticker + '.csv'
-    data = pd.read_csv(ticker, parse_dates=['Date'])  # import historical data in pandas dataframe
-    data = data.sort_values(by='Date')
-    data.set_index('Date', inplace=True)
-
-    data['Returns'] = data['Adj Close'].pct_change()  # create a column of daily returns
-    stock_returns = data['Returns'].dropna()  # remove entries with N/A dat
-    return stock_returns
-
-
 # For each stock in the portfolio, generate a column of its daily returns and store it in the stock return matrix
 for stock in stock_port:
     stock_ret_mat[stock] = stockReturnCol(stock)
 
 # Generate a correlation matrix of the stock returns using the Pearson method
 corr_matrix = stock_ret_mat.corr(method='pearson')
-
-
-def randomPortfolios(num_portfolios, mean_returns, cov_matrix, risk_free_rate):
-    results = np.zeros((3, num_portfolios))
-    weights_record = []
-    for i in range(num_portfolios):
-        weights = np.random.random_sample(len(stock_port))
-        weights /= np.sum(weights)
-        weights_record.append(weights)
-        portfolio_return = np.sum(mean_returns * weights) * 252
-        portfolio_std_dev = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights))) * np.sqrt(252)
-        results[0, i] = portfolio_std_dev
-        results[1, i] = portfolio_return
-        results[2, i] = (portfolio_return - risk_free_rate) / portfolio_std_dev
-    return results, weights_record
-
 
 returns = stock_ret_mat  # matrix, 250 daily returns for each stock
 mean_returns = returns.mean()  # array, 1 average return for each stock
@@ -74,7 +73,6 @@ std_sharpe = results[0, msr_index]
 return_sharpe = results[1, msr_index]
 print(std_sharpe)
 print(return_sharpe)
-
 msr_array = weights[msr_index].round(4)
 msr_alloc = pd.DataFrame(msr_array, index=stock_port, columns=['Allocation']).T
 #print(msr_alloc)
@@ -82,7 +80,6 @@ msr_alloc = pd.DataFrame(msr_array, index=stock_port, columns=['Allocation']).T
 msr_alloc = msr_alloc.T.sort_values(by=['Allocation'], ascending=False)
 #print(msr_alloc)
 #print(type(msr_alloc))
-
 msr_alloc.plot.pie(subplots=True,
                    autopct='%1.2f%%',
                    legend=False,
@@ -98,7 +95,6 @@ return_gmv = results[1, gmv_index]
 gmv_array = weights[gmv_index].round(4)
 gmv_alloc = pd.DataFrame(gmv_array, index=stock_port, columns=['Allocation']).T
 gmv_alloc = gmv_alloc.T.sort_values(by=['Allocation'], ascending=True)
-
 gmv_alloc.plot.pie(subplots=True,
                    autopct='%1.2f%%',
                    legend=False,
@@ -106,7 +102,6 @@ gmv_alloc.plot.pie(subplots=True,
                    labeldistance=1.3)
 plt.title('Allocation: Global Minimum Volatility')
 plt.show()
-
 plt.figure()
 plt.scatter(x=results[0, :],
             y=results[1, :],
